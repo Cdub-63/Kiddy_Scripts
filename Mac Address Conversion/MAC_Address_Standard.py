560e56356c8b6a657d21c554a090f70307f0b753
Cdub0956 <44756128+Cdub0956@users.noreply.github.com>
2019-12-02 17:31:08 -0600

    Add files via upload

---
+++ Mac Address Conversion/MAC_Address_Standard.py
@@ -0,0 +1,25 @@
+def mac_conv(mac, sep=':'):
+	#split mac address into 3 groups
+	mac = mac.upper()
+	mac_parts = mac.split(sep)
+	return "{0[0]}{0[1]}.{0[2]}{0[3]}.{0[4]}{0[5]}".format(mac_parts)
+
+	#join groups into a single MAC
+	final_mac = '.'.join([gr1, gr2, gr3])
+
+	#print final MAC address
+	return(final_mac)
+
+print('=' * 80)
+print("[<Interesting MACs Listed>]".center(60,' '))
+print('=' * 80)
+
+#Open file with MAC addresses and convert them
+with open('mac.txt', 'r') as f:
+	for mac in f.readlines():
+		mac2 = mac_conv(mac)
+		print("{} =======> {}".format(mac, mac2))
+
+print('=' * 80)
+print('[<END>]'.center(60,' '))
+print('=' * 80)
